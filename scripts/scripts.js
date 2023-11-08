2c598ea1cbc3f654674ddf8867044c4b2d03d59d
Emad Hmady <emadhmady0@gmail.com>
2023-11-08 23:16:26 +0200

    creating createItem function that will return an 'li' item with buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,6 +12,17 @@ function createHtmlTagWithId(tag_name,id_name){
     return tag
 }
 
+function createItem(task_name){
+    const lst_item = createHtmlTagWithClassName("li","task-element")
+    const item_name_div = createHtmlTagWithClassName("div","task-text")
+    const edit_item_btn = createHtmlTagWithId("button","edit-task-btn")
+    const delete_item_btn = createHtmlTagWithId("button","delete-task-btn")
+    item_name_div.innerHTML = task_name.value
+    edit_item_btn.innerHTML = "edit"
+    delete_item_btn.innerHTML = "delete"
+    lst_item.append(item_name_div,edit_item_btn,delete_item_btn)
+    return lst_item
+}
 
 
 
